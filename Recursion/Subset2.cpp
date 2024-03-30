041522ddd06a9517a25173cb33fe9c434379e966
mayankpallai <mayankpallai@gmail.com>
2024-03-30 23:05:37 +0800

    subset ii problem

--- Recursion/Subset2.cpp
+++ Recursion/Subset2.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <set>
 
 using namespace std;
 
@@ -18,6 +19,34 @@ public:
 
             sort(temp.begin(), temp.end());
             if(count(ans.begin(), ans.end(), temp) == 0) ans.push_back(temp);
+        }
+
+        return ans;
+    }
+};
+
+//optimal approach
+class Solution {
+public:
+    void subsetHelper(vector<int> &nums, vector<int> subset, set<vector<int>> &res, int index){
+        if(index == nums.size()){
+            sort(subset.begin(), subset.end());
+            res.insert(subset);
+            return;
+        }
+
+        subset.push_back(nums[index]);
+        subsetHelper(nums, subset, res, index + 1);
+        subset.pop_back();
+        subsetHelper(nums, subset, res, index + 1);
+    }
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        vector<vector<int>> ans;
+        set<vector<int>> res;
+        vector<int> subset;
+        subsetHelper(nums, subset, res, 0);
+        for(auto it = res.begin(); it != res.end(); it++){
+            ans.push_back(*it);
         }
 
         return ans;
