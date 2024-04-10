010f84dfd0850fcd879f23d7d8187c73601805a1
mayankpallai <mayankpallai@gmail.com>
2024-04-11 03:06:55 +0800

    rotated array

--- BinarySearch/SingleElementSorted.cpp
+++ BinarySearch/SingleElementSorted.cpp
@@ -39,38 +39,38 @@ int singleNonDuplicate(vector<int> &arr)
     return ans;
 }
 
-// binary search approach
-int singleNonDuplicate(vector<int> &arr)
+// binary search approach (most optimal)
+class Solution
 {
-    int n = arr.size();
-
-    if (n == 1)
-        return arr[0];
-    if (arr[0] != arr[1])
-        return arr[0];
-    if (arr[n - 1] != arr[n - 2])
-        return arr[n - 1];
-
-    int low = 1, high = n - 2;
-    while (low <= high)
+public:
+    int singleNonDuplicate(vector<int> &nums)
     {
-        int mid = (low + high) / 2;
+        int n = nums.size();
 
-        if (arr[mid] != arr[mid + 1] && arr[mid] != arr[mid - 1])
+        if (n == 1)
+            return nums[0];
+        if (nums[0] != nums[1])
+            return nums[0];
+        if (nums[n - 1] != nums[n - 2])
+            return nums[n - 1];
+
+        int low = 1, high = n - 2;
+
+        while (low <= high)
         {
-            return arr[mid];
+            int mid = (low + high) / 2;
+
+            if (nums[mid] != nums[mid - 1] && nums[mid] != nums[mid + 1])
+                return nums[mid];
+
+            if ((mid % 2 == 0 && nums[mid] == nums[mid + 1]) ||
+                (mid % 2 == 1 && nums[mid] == nums[mid - 1]))
+                low = mid + 1;
+
+            else
+                high = mid - 1;
         }
 
-        if ((mid % 2 == 1 && arr[mid] == arr[mid - 1]) ||
-            (mid % 2 == 0 && arr[mid] == arr[mid + 1]))
-        {
-            low = mid + 1;
-        }
-        else
-        {
-            high = mid - 1;
-        }
+        return -1;
     }
-
-    return -1;
-}
+};
