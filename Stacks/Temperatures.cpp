0ffc857f62d0658da2fe61a90229dcffda0247cb
mayankpallai <mayankpallai@gmail.com>
2024-05-28 22:24:38 +0800

    stack questions

---
+++ Stacks/Temperatures.cpp
@@ -0,0 +1,26 @@
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        stack<int> st;
+        vector<int> result(temperatures.size(), 0);
+
+        for (int i = 0; i < temperatures.size(); i++)
+        {
+            while (!st.empty() && temperatures[i] > temperatures[st.top()])
+            {
+                int index = st.top();
+                st.pop();
+                result[index] = i - index;
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
