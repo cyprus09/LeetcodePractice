7f1b2979901091d6ec5f11db4956fc4d8caa200f
mayankpallai <mayankpallai@gmail.com>
2024-06-06 23:21:56 +0800

    find duplicate slow fast approach

--- Arrays/FindDuplicate.cpp
+++ Arrays/FindDuplicate.cpp
@@ -2,12 +2,17 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-    int findDuplicate(vector<int>& nums) {
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = i + 1; j < nums.size(); j++){
-                if(nums[i] == nums[j]){
+    int findDuplicate(vector<int> &nums)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[i] == nums[j])
+                {
                     return nums[i];
                 }
             }
@@ -17,19 +22,24 @@ public:
     }
 };
 
-//use this for nlogn approach
-class Solution {
+// use this for nlogn approach
+class Solution
+{
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate(vector<int> &nums)
+    {
         vector<int> newVector;
-        for(int i : nums){
-            newVector.push_back(i); 
+        for (int i : nums)
+        {
+            newVector.push_back(i);
         }
 
         sort(newVector.begin(), newVector.end());
 
-        for(int j = 1; j < newVector.size(); j++){ 
-            if(newVector[j - 1] == newVector[j]){
+        for (int j = 1; j < newVector.size(); j++)
+        {
+            if (newVector[j - 1] == newVector[j])
+            {
                 return newVector[j];
             }
         }
@@ -38,16 +48,22 @@ public:
     }
 };
 
-//best approach
-class Solution {
+// best approach
+class Solution
+{
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate(vector<int> &nums)
+    {
         int n = nums.size();
         vector<int> freq(n + 1, 0);
-        for (int i = 0; i < n; i++) {
-            if (freq[nums[i]] == 0) {
+        for (int i = 0; i < n; i++)
+        {
+            if (freq[nums[i]] == 0)
+            {
                 freq[nums[i]] += 1;
-            } else {
+            }
+            else
+            {
                 return nums[i];
             }
         }
@@ -55,24 +71,28 @@ public:
     }
 };
 
-//cyclic linked list approach
-class Solution {
+// cyclic linked list approach (Floyd's Algorithm)
+class Solution
+{
 public:
-    int findDuplicate(vector<int>& nums) {
+    int findDuplicate(vector<int> &nums)
+    {
         int slow = nums[0];
         int fast = nums[nums[0]];
 
-        while(slow != fast){
+        while (slow != fast)
+        {
             slow = nums[slow];
             fast = nums[nums[fast]];
         }
 
-        slow = 0;
+        int slow2 = 0;
 
-        while(slow != fast){
+        while (slow != slow2)
+        {
             slow = nums[slow];
-            fast = nums[fast];
+            slow2 = nums[slow2];
         }
-        return slow;
+        return slow2;
     }
 };
