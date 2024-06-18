e53cf45c83a46347de568cd1938e2ead53cbdac6
mayankpallai <mayankpallai@gmail.com>
2024-06-18 22:22:16 +0800

    priority queue

---
+++ PriorityQueues/KthLargest.cpp
@@ -0,0 +1,30 @@
+#include <vector>
+#include <priority_queue>
+
+using namespace std;
+
+class KthLargest {
+private:
+    int k;
+    priority_queue<int, vector<int>, greater<int>> pq;
+
+public:
+    KthLargest(int k, vector<int>& nums) {
+        this->k = k;
+        for (int i = 0; i < nums.size(); i++) {
+            pq.push(nums[i]);
+        }
+
+        while (pq.size() > this->k) {
+            pq.pop();
+        }
+    }
+
+    int add(int val) {
+        pq.push(val);
+        if (pq.size() > k) {
+            pq.pop();
+        }
+        return pq.top();
+    }
+};
