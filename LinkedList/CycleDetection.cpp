de306d435e2e0f1137a8910e4f48224bd56b02da
mayankpallai <mayankpallai@gmail.com>
2024-07-13 23:45:28 +0800

    graph questions

--- LinkedList/CycleDetection.cpp
+++ LinkedList/CycleDetection.cpp
@@ -1,34 +1,42 @@
 #include "linkedlist.h"
 
-//uses extra space
-class Solution {
+// uses extra space
+class Solution
+{
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode* temp = head;
-        unordered_set<ListNode*> st;
-        while(temp != NULL){
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *temp = head;
+        unordered_set<ListNode *> st;
+        while (temp != NULL)
+        {
             st.insert(temp);
             temp = temp->next;
-            if(st.find(temp) != st.end()){
+            if (st.find(temp) != st.end())
+            {
                 return true;
-            }            
+            }
         }
         return false;
     }
 };
 
-//slow-fast pointer approach
-class Solution {
+// slow-fast pointer approach
+class Solution
+{
 public:
-    bool hasCycle(ListNode *head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-       
-        while(fast != NULL && fast->next != NULL){
+    bool hasCycle(ListNode *head)
+    {
+        ListNode *slow = head;
+        ListNode *fast = head;
+
+        while (fast != NULL && fast->next != NULL)
+        {
             slow = slow->next;
             fast = fast->next->next;
 
-            if(slow == fast){
+            if (slow == fast)
+            {
                 return true;
             }
         }
