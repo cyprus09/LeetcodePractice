e215873c6ef68dc6d8e5f4d90540479c812f93aa
mayankpallai <mayankpallai@gmail.com>
2024-08-02 00:57:28 +0800

    string chain question

--- DynamicProgramming2D/LongestIncSub/PrintLongestIncreasingSub.cpp
+++ DynamicProgramming2D/LongestIncSub/PrintLongestIncreasingSub.cpp
@@ -1,50 +1,61 @@
 #include <vector>
+#include <iostream>
 
 using namespace std;
-class Solution
-{
-public:
-  vector<int> longestIncreasingSubsequence(int n, vector<int> &arr)
-  {
-    vector<int> dp(n, 1);
-    vector<int> hash(n, 1);
 
-    for (int i = 0; i <= n - 1; i++)
-    {
-      hash[i] = i;
-      for (int prev_index = 0; prev_index <= i - 1; prev_index++)
-      {
-        if (arr[prev_index] < arr[i] && 1 + dp[prev_index] > dp[i])
-        {
-          dp[i] = 1 + dp[prev_index];
-          hash[i] = prev_index;
+class Solution {
+  public:
+    vector<int> longestIncreasingSubsequence(int n, vector<int>& arr) {
+        vector<int> dp(n, 1);
+        vector<int> hash(n, 1);
+        
+        for(int i = 0; i <= n - 1; i++){
+            hash[i] = i;
+            for(int prev = 0; prev <= i - 1; prev ++){
+                if(arr[prev] < arr[i] && 1 + dp[prev] > dp[i]){
+                    dp[i] = 1 + dp[prev];
+                    hash[i] = prev;
+                }
+            }
         }
-      }
+        
+        int ans = -1, lastIndex = -1;
+        for(int i = 0; i <= n - 1; i++){
+            if(dp[i] > ans){
+                ans = dp[i];
+                lastIndex = i;
+            }
+        }
+        
+        vector<int> temp;
+        temp.push_back(arr[lastIndex]);
+        
+        while(hash[lastIndex] != lastIndex){
+            lastIndex = hash[lastIndex];
+            temp.push_back(arr[lastIndex]);
+        }
+        
+        reverse(temp.begin(), temp.end());
+        
+        return temp;
     }
-    
-    int ans = -1;
-    int lastIndex = -1;
-
-    for (int i = 0; i <= n - 1; i++)
-    {
-      if (dp[i] > ans)
-      {
-        ans = dp[i];
-        lastIndex = i;
-      }
-    }
-
-    vector<int> temp;
-    temp.push_back(arr[lastIndex]);
-
-    while (hash[lastIndex] !=
-           lastIndex)
-    {
-      lastIndex = hash[lastIndex];
-      temp.push_back(arr[lastIndex]);
-    }
-
-    reverse(temp.begin(), temp.end());
-    return temp;
-  }
 };
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+        vector<int> arr(N);
+        for (int i = 0; i < N; i++) {
+            cin >> arr[i];
+        }
+        Solution obj;
+        vector<int> res = obj.longestIncreasingSubsequence(N, arr);
+        for (auto x : res)
+            cout << x << " ";
+        cout << "\n";
+    }
+    return 0;
+}
--- Maths/SpiralMatrix.cpp
+++ Maths/SpiralMatrix.cpp
@@ -8,34 +8,33 @@ public:
   vector<int> spiralOrder(vector<vector<int>> &matrix)
   {
     int top = 0, left = 0;
-    int bottom = matrix.size() - 1, right = matrix[0].size() - 1;
-    vector<int> answer;
+    int bottom = matrix.size(), right = matrix[0].size();
+    vector<int> result;
 
-    while (top <= bottom && left <= right)
+    while (top < bottom && left < right)
     {
-      for (int i = left; i <= right; i++)
-        answer.push_back(matrix[top][i]);
+      for (int i = left; i < right; i++)
+        result.push_back(matrix[top][i]);
       top++;
 
-      for (int i = top; i <= bottom; i++)
-        answer.push_back(matrix[i][right]);
+      for (int i = top; i < bottom; i++)
+        result.push_back(matrix[i][right - 1]);
       right--;
 
-      if (top <= bottom)
+      if (top < bottom)
       {
-        for (int i = right; i >= left; i--)
-          answer.push_back(matrix[bottom][i]);
-        bottom--;
+        for (int i = right - 1; i >= left; i--)
+          result.push_back(matrix[bottom - 1][i]);
       }
+      bottom--;
 
-      if (left <= right)
+      if (left < right)
       {
-        for (int i = bottom; i >= top; i--)
-          answer.push_back(matrix[i][left]);
+        for (int i = bottom - 1; i >= top; i--)
+          result.push_back(matrix[i][left]);
+        left++;
       }
-      left++;
     }
-
-    return answer;
+    return result;
   }
 };
