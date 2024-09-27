402791834d29b4b3dd5074365a285f002e4755b5
mayankpallai <mayankpallai@gmail.com>
2024-09-28 01:34:37 +0800

    array problems

---
+++ Arrays/RemoveElement.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+
+using namespace std;
+
+class Solution
+{
+public:
+  int removeElement(vector<int> &nums, int val)
+  {
+    int count = 0;
+    for (int num : nums)
+    {
+      if (num != val)
+      {
+        nums[count] = num;
+        count++;
+      }
+    }
+
+    return count;
+  }
+};
